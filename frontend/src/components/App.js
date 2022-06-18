1bc34cee87310cabe104b940ff1898b55e4d0df0
Kara Rizzardi <kara.rizzardi@keysight.com>
2022-06-18 16:52:20 -0700

    Connect frontend react to backend django

---
+++ frontend/src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { render } from "react-dom";
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hi there</h1>
+        </div>
+    );
+}
+
+export default App;
+const appDiv = document.getElementById("app");
+render(<App />, appDiv);
