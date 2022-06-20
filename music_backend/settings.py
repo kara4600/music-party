99557087b2e0569586a3ab1278b4522fb6334ccc
Kara Rizzardi <36972680+kara4600@users.noreply.github.com>
2022-06-20 09:51:18 -0700

    Update settings.py

--- music_backend/settings.py
+++ music_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k+h&lq5qek*)50t8mij3)@i)hn40k0i%7@86-^m31ptfjc*u2n'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
