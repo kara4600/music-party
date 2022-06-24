a9b49e1d709a9af7fb9df02c413428379b5fa9bf
kara4600 <kara178@comcast.net>
2022-06-24 15:26:40 -0700

    Add RoomPage.js

---
+++ frontend/src/pages/RoomPage.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+const RoomPage = () => {
+  const defaultVotes = 2;
+  let params = useParams();
+  let code = params.code;
+
+  const [state, setState] = useState({
+    votesToSkip: defaultVotes,
+    guestCanPause: true,
+    isHost: true,
+    roomCode: code,
+  });
+
+  let getRoomInfo = async () => {
+    await fetch(`/api/get-room?code=${state.roomCode}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setState({
+          ...state,
+          votesToSkip: data.votes_to_skip,
+          guestCanPause: data.guest_can_pause,
+          isHost: data.is_host,
+        });
+      });
+  };
+
+  useEffect(() => {
+    getRoomInfo();
+  }, [state]);
+
+  return (
+    <div>
+      <h1>Room code: {state.roomCode}</h1>
+      <p>Votes: {state.votesToSkip}</p>
+      <p>Guest can Pause: {state.guestCanPause.toString()}</p>
+      <p>Host: {state.isHost.toString()}</p>
+    </div>
+  );
+};
+
+export default RoomPage;
