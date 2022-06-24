b7763c28096f68e4c5ec175074051ba6ec750477
kara4600 <kara178@comcast.net>
2022-06-24 15:33:42 -0700

    add get-room api endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,11 @@
 from xml.etree.ElementInclude import include
 from django.urls import path
-from .views import CreateRoomView, RoomView
+from .views import CreateRoomView, GetRoom, RoomView
 
 # api endpoint
 urlpatterns = [
     # Calls main function from views.py
     path('', RoomView.as_view()),
-    path('create-room', CreateRoomView.as_view())
+    path('create-room', CreateRoomView.as_view()),
+    path('get-room', GetRoom.as_view())
 ]
