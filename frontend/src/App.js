70aba461bc2d5a588b2684a6e8890723981dc845
kara4600 <kara178@comcast.net>
2022-06-24 15:26:58 -0700

    Remove unnecessary imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import AboutPage from './pages/AboutPage';
 import HomePage from './pages/HomePage';
 import CreateRoomPage from './pages/CreateRoomPage';
+import RoomPage from './pages/RoomPage';
 
 const App = () => {
-    return (
-        <Router>
-            <div>
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/create" element={<CreateRoomPage />} />
-                </Routes>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/create" element={<CreateRoomPage />} />
+          <Route path="/room/:code" element={<RoomPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
